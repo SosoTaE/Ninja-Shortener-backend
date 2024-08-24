b9a281f1dc4ce4ba30b3bb62838d9ddd4075af28
SosoT <sosotae1615@gmail.com>
2024-08-24 14:43:09 +0400

    added new way to check url validation and added two env variables

--- main.go
+++ main.go
@@ -4,16 +4,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"log"
+	"os"
 )
 
 func main() {
 	app := fiber.New()
 
-	allowedOrigin := "https://sulkhans.github.io"
-	//allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
-	//if allowedOrigin == "" {
-	//	panic("ALLOWED_ORIGIN environment variable is not set")
-	//}
+	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+	if allowedOrigin == "" {
+		panic("ALLOWED_ORIGIN environment variable is not set")
+	}
 
 	corsConfig := cors.Config{
 		AllowOrigins:     allowedOrigin, // Replace with your actual frontend domain
